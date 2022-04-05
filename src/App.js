a2e4f210c26a5f7d580ec48d4741d3334391aba1
gerarjon <gerarsuaverdez@gmail.com>
2022-04-05 16:42:00 -0700

    test 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column'>
+            column 1
+          </div>
+          <div className='column'>
+            column 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
