d9ee85a77457904c38cc0f816a30eb394534bd0f
gerarjon <gerarsuaverdez@gmail.com>
2022-04-06 19:52:52 -0700

    changed OrderPage -> MakeTable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bulma/css/bulma.min.css';
 import OrderPage from './pages/OrderPage';
+import MakeTable from './pages/MakeTable';
 
 function App() {
   return (
     <>
-      <OrderPage />
+      <MakeTable />
     </>
   );
 }
