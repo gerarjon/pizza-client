d393eb04f9c2a5678c495df2b0da6f6e69f0fa10
unknown <jvn1567@gmail.com>
2022-07-16 23:02:40 -0700

    fixed to work in react

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,53 +1,25 @@
-import React, { useState, useRef, useContext, useEffect } from 'react';
-import MenuItem from "../components/MenuItem";
+import React from 'react';
+import CustomerInfo from '../CustomerInfo/index.js';
+import MenuItem from '../MenuItem/index.js';
 
-const Order = () => {
-    const [customer, setCustomer] = useState(null);
-    const [menuItems, setMenuItems] = useState([]);
 
-    insertHelper = (menuItem, left, right) => {
-        const mid = Math.floor((left + right) / 2);
-        const compare = menuItems[mid].compareTo(menuItem);
-        if (left > right) {
-            menuItems.splice(left, 0, menuItem);
-        } else if (compare == 0) {
-            var item = menuItems[mid];
-            item.setCount(item.count + 1);
-        } else if (compare > 0) {
-            insertHelper(menuItem, mid + 1, right);
-        } else {
-            insertHelper(menuItem, min, mid - 1);
-        }
-    }
+const Order = (props) => {
 
-    insert = (menuItem) => {
-        insert(menuItem, 0, menuItems.size());
+  const [customer, itemList] = props;
 
-    }
+  const listItems = (itemList) => {
+    return itemList.map(item => {
+      return <MenuItem item={item} />
+    })
+  }
 
-    removeHelper = (menuItem, left, right) => {
-        const mid = Math.floor((left + right) / 2);
-        const compare = menuItems[mid].compareTo(menuItem);
-        if (left > right) {
-            return false;
-        } else if (compare == 0) {
-            var item = menuItems[mid];
-            if (item.count > 1) {
-                setCount(item.count - 1);
-            } else {
-                menuItems.splice(mid, 1);
-            }
-            return true;
-        } else if (compare > 0) {
-            return removeHelper(menuItem, mid + 1, right);
-        } else {
-            return removeHelper(menuItem, min, mid - 1);
-        }
-    }
+  return (
+    <div>
+      <CustomerInfo customer={customer} />
+      {listItems(itemList)}
+    </div>
+  )
 
-    remove = (menuItem) => {
-        return remove(menuItem, 0, menuItems.size());
-    }
 }
 
 export default Home;
