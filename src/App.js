f274f223c1c1ea426b1deccab250a06a99d98912
gerarjon <gerarsuaverdez@gmail.com>
2022-07-21 16:35:29 -0700

    created sample menu page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import BuildPizza from './pages/BuildPizza';
 import MakeTable from './pages/MakeTable';
 import NoPage from './pages/NoPage';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
+import MenuList from './components/MenuList';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+      
           <Route index element={<Home />} />
-          <Route path="build-pizza" element={<BuildPizza />} />
           <Route path="maketable" element={<MakeTable />} />
+          <Route path="menu" element={<Menu />}>
+            <Route index element={<MenuList />} />
+            <Route path="build-pizza" element={<BuildPizza />} />
+          </Route>
           <Route path="*" element={<NoPage />} />
-        </Route>
+  
       </Routes>
     </BrowserRouter>
   );
